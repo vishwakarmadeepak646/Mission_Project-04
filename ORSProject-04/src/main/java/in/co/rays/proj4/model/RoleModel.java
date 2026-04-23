a9ed798a55718a72a505685175fe268067bac3a7
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-04-23 12:59:39 +0530

    Few Methods added

--- ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/model/RoleModel.java
@@ -1,5 +1,147 @@
 package in.co.rays.proj4.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import in.co.rays.proj4.bean.RoleBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.exception.DatabaseException;
+import in.co.rays.proj4.util.JDBCDataSource;
+
 public class RoleModel {
+
+	public Integer nextPk() throws DatabaseException {
+
+		Connection conn = null;
+		int pk = 0;
+		try {
+
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_role");
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				pk = rs.getInt(1);
+			}
+
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			throw new DatabaseException("Exception : Exception in getting PK");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return pk + 1;
+	}
+
+	public long add(RoleBean bean) throws ApplicationException {
+		Connection conn = null;
+		int pk = 0;
+
+		try {
+			pk = nextPk();
+
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("insert into st_role values(?,?,?,?,?,?,?)");
+
+			pstmt.setLong(1, pk);
+			pstmt.setString(2, bean.getName());
+			pstmt.setString(3, bean.getDescription());
+			pstmt.setString(4, bean.getCreatedBy());
+			pstmt.setString(5, bean.getModifiedBy());
+			pstmt.setTimestamp(6, bean.getCreatedDatetime());
+			pstmt.setTimestamp(7, bean.getModifiedDatetime());
+
+			pstmt.executeUpdate();
+			conn.commit();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : add rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception : Exception in add Role");
+
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return pk;
+	}
+
+	public void update(RoleBean bean) throws ApplicationException {
+		Connection conn = null;
+
+		try {
+
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement(
+					"update st_role set name = ?, description 0 ?, created_by = ?, modified_by = ?, created_datetime = ? , modified_datetime = ? where id = ?");
+
+			pstmt.setString(1, bean.getName());
+			pstmt.setString(2, bean.getDescription());
+			pstmt.setString(3, bean.getCreatedBy());
+			pstmt.setString(4, bean.getModifiedBy());
+			pstmt.setTimestamp(5, bean.getCreatedDatetime());
+			pstmt.setTimestamp(6, bean.getModifiedDatetime());
+			pstmt.setLong(7, bean.getId());
+
+			pstmt.executeUpdate();
+			conn.commit();
+			pstmt.close();
+			System.out.println("Role Updated Successfully...!!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : add rollback exception" + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in updating Role ");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+	}
+	
+	public void delete(RoleBean bean ) throws ApplicationException {
+		Connection conn = null;
+		
+		try {
+			
+			JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("delete from st_role where id = ?");
+			pstmt.setLong(1, bean.getId());
+			
+			pstmt.executeUpdate();
+			
+			conn.commit();
+			pstmt.close();
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			}catch(Exception ex) {
+				throw new ApplicationException("Exception :  Delete rollback exception" + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in deleting role");
+		}finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+	}
 
 }
