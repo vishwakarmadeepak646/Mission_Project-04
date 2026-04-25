1bc9ec361908edde0dd8bad00973daa0bc99f87b
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-04-25 12:36:30 +0530

    Done

--- ORSProject-04/src/main/java/in/co/rays/proj4/Test/CollegeTest.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/Test/CollegeTest.java
@@ -1,6 +1,9 @@
 package in.co.rays.proj4.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.sql.Timestamp;
 
 import in.co.rays.proj4.bean.CollegeBean;
@@ -9,7 +12,12 @@ import in.co.rays.proj4.model.CollegeModel;
 public class CollegeTest {
 
 	public static void main(String[] args) {
-		getAdd();
+		// getAdd();
+		// getUpdate();
+		// getDelete();
+		// findByPK();
+		// findByName();
+		search();
 	}
 
 	public static void getAdd() {
@@ -30,6 +38,135 @@ public class CollegeTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void getUpdate() {
+		try {
+
+			CollegeBean bean = new CollegeBean();
+			CollegeModel m = new CollegeModel();
+			bean.setId(2);
+			bean.setName("LNCT");
+			bean.setAddress("Bhopal");
+			bean.setState("Madhya Pradesh");
+			bean.setCity("Near TCS Bhopal");
+			bean.setPhoneNo("9900234576");
+			bean.setCreatedBy("Faculty");
+			bean.setModifiedBy("Faculty");
+			bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+			bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+			m.update(bean);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getDelete() {
+		try {
+
+			CollegeBean bean = new CollegeBean();
+			CollegeModel m = new CollegeModel();
+			bean.setId(2);
+
+			m.delete(bean);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void findByPK() {
+
+		try {
+			CollegeBean bean = new CollegeBean();
+			CollegeModel m = new CollegeModel();
+
+			bean = m.findByPk(1);
+
+			if (bean != null) {
+				System.out.println(bean.getId());
+				System.out.println(bean.getName());
+				System.out.println(bean.getAddress());
+				System.out.println(bean.getState());
+				System.out.println(bean.getCity());
+				System.out.println(bean.getPhoneNo());
+				System.out.println(bean.getCreatedBy());
+				System.out.println(bean.getModifiedBy());
+				System.out.println(bean.getCreatedDatetime());
+				System.out.println(bean.getModifiedDatetime());
+				System.out.println("------------------------");
+			} else {
+				System.out.println("No record found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void findByName() {
+
+		try {
+			CollegeBean bean = new CollegeBean();
+			CollegeModel m = new CollegeModel();
+
+			bean = m.findByName("LNCT");
+
+			if (bean != null) {
+				System.out.println(bean.getId());
+				System.out.println(bean.getName());
+				System.out.println(bean.getAddress());
+				System.out.println(bean.getState());
+				System.out.println(bean.getCity());
+				System.out.println(bean.getPhoneNo());
+				System.out.println(bean.getCreatedBy());
+				System.out.println(bean.getModifiedBy());
+				System.out.println(bean.getCreatedDatetime());
+				System.out.println(bean.getModifiedDatetime());
+				System.out.println("------------------------");
+			} else {
+				System.out.println("No record found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void search() {
+		CollegeBean bean = new CollegeBean();
+		CollegeModel model = new CollegeModel();
+		List<CollegeBean> list = new ArrayList<CollegeBean>();
+		int pageNo = 1;
+		int pageSize = 5;
+		
+	//	bean.setId(8);
+	//	bean.setName("s");
+	//	bean.setAddress("bhopal");
+	//	bean.setCity("bh");
+	//	bean.setPhoneNo("9876543212");
+		
+		list = model.search(bean, pageNo, pageSize);
+		Iterator<CollegeBean> it = list.iterator();
+
+		while (it.hasNext()) {
+			bean = it.next();
+
+			System.out.println(bean.getId());
+			System.out.println(bean.getName());
+			System.out.println(bean.getAddress());
+			System.out.println(bean.getState());
+			System.out.println(bean.getCity());
+			System.out.println(bean.getPhoneNo());
+			System.out.println(bean.getCreatedBy());
+			System.out.println(bean.getModifiedBy());
+			System.out.println(bean.getCreatedDatetime());
+			System.out.println(bean.getModifiedDatetime());
+			System.out.println("------------------------");
+		}
+
 	}
 
 }
--- ORSProject-04/src/main/java/in/co/rays/proj4/model/CollegeModel.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/model/CollegeModel.java
@@ -3,6 +3,8 @@ package in.co.rays.proj4.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import in.co.rays.proj4.bean.CollegeBean;
 import in.co.rays.proj4.exception.ApplicationException;
@@ -17,7 +19,7 @@ public class CollegeModel {
 		try {
 
 			conn = JDBCDataSource.getConnection();
-			
+
 			PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_college");
 			ResultSet rs = pstmt.executeQuery();
 
@@ -61,7 +63,7 @@ public class CollegeModel {
 
 			pstmt.executeUpdate();
 			conn.commit();
-			
+			System.out.println("College added successfully...");
 			pstmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -78,4 +80,213 @@ public class CollegeModel {
 
 		return pk;
 	}
+
+	public void update(CollegeBean bean) throws ApplicationException {
+
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement(
+					"update st_college set name =?, address = ?, state = ?, city = ?, phone_no =? ,created_by = ?, modified_by = ?, created_dateTime = ? , modified_datetime = ? where id =?");
+
+			pstmt.setString(1, bean.getName());
+			pstmt.setString(2, bean.getAddress());
+			pstmt.setString(3, bean.getState());
+			pstmt.setString(4, bean.getCity());
+			pstmt.setString(5, bean.getPhoneNo());
+			pstmt.setString(6, bean.getCreatedBy());
+			pstmt.setString(7, bean.getModifiedBy());
+			pstmt.setTimestamp(8, bean.getCreatedDatetime());
+			pstmt.setTimestamp(9, bean.getModifiedDatetime());
+			pstmt.setLong(10, bean.getId());
+
+			pstmt.executeUpdate();
+
+			conn.commit();
+			System.out.println("College updated successfully...");
+			pstmt.close();
+
+		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				throw new ApplicationException("Exception : Update rollback Excepiton" + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in updating College...");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+	}
+
+	public void delete(CollegeBean bean) throws ApplicationException {
+		Connection conn = null;
+
+		try {
+
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt = conn.prepareStatement("delete from st_college where id = ?");
+			pstmt.setLong(1, bean.getId());
+			pstmt.executeUpdate();
+
+			conn.commit();
+			System.out.println("College deleted successfully...");
+			pstmt.close();
+
+		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				throw new ApplicationException("Exception : Deleting rollback Exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in deleting college...");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+	}
+
+	public CollegeBean findByPk(long pk) throws ApplicationException {
+		CollegeBean bean = null;
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select * from st_college where id = ?");
+			pstmt.setLong(1, pk);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new CollegeBean();
+
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setAddress(rs.getString(3));
+				bean.setState(rs.getString(4));
+				bean.setCity(rs.getString(5));
+				bean.setPhoneNo(rs.getString(6));
+				bean.setCreatedBy(rs.getString(7));
+				bean.setModifiedBy(rs.getString(8));
+				bean.setCreatedDatetime(rs.getTimestamp(9));
+				bean.setModifiedDatetime(rs.getTimestamp(10));
+
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			throw new ApplicationException("Exception : Exception in getting College by pk");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return bean;
+
+	}
+
+	public CollegeBean findByName(String name) throws ApplicationException {
+		Connection conn = null;
+		CollegeBean bean = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select * from st_college where name = ?");
+			pstmt.setString(1, name);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new CollegeBean();
+
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setAddress(rs.getString(3));
+				bean.setState(rs.getString(4));
+				bean.setCity(rs.getString(5));
+				bean.setPhoneNo(rs.getString(6));
+				bean.setCreatedBy(rs.getString(7));
+				bean.setModifiedBy(rs.getString(8));
+				bean.setCreatedDatetime(rs.getTimestamp(9));
+				bean.setModifiedDatetime(rs.getTimestamp(10));
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			throw new ApplicationException("Exception :  Exception in getting College by Name");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		return bean;
+
+	}
+
+	public List<CollegeBean> search(CollegeBean bean, int PageNo, int PageSize) {
+
+		StringBuffer sql = new StringBuffer("select * from st_college where 1=1");
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" and id = " + bean.getId());
+			}
+
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" and name like'" + bean.getName() + "%'");
+			}
+			if (bean.getAddress() != null && bean.getAddress().length() > 0) {
+				sql.append(" and address like'" + bean.getAddress() + "%'");
+			}
+			if (bean.getState() != null && bean.getState().length() > 0) {
+				sql.append(" and state like'" + bean.getState() + "%'");
+			}
+			if (bean.getCity() != null && bean.getCity().length() > 0) {
+				sql.append(" and city like'" + bean.getCity() + "%'");
+			}
+			if (bean.getPhoneNo() != null && bean.getPhoneNo().length() > 0) {
+				sql.append(" and phone_no like'" + bean.getPhoneNo() +"%'");
+			}
+		}
+
+		if (PageNo > 0) {
+			PageNo = (PageNo - 1) * PageSize;
+			sql.append(" limit " + PageNo + " , " + PageSize);
+		}
+
+		List<CollegeBean> list = new ArrayList<CollegeBean>();
+		Connection conn = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				bean = new CollegeBean();
+
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setAddress(rs.getString(3));
+				bean.setState(rs.getString(4));
+				bean.setCity(rs.getString(5));
+				bean.setPhoneNo(rs.getString(6));
+				bean.setCreatedBy(rs.getString(7));
+				bean.setModifiedBy(rs.getString(8));
+				bean.setCreatedDatetime(rs.getTimestamp(9));
+				bean.setModifiedDatetime(rs.getTimestamp(10));
+				list.add(bean);
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return list;
+	}
+
 }
