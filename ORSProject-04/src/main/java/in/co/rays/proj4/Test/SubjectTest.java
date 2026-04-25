d9109bbcea705196e7d93e2d1033288ebfb51fb3
vishwakarmadeepak646 <vishwakarmadeepak646@gmail.com>
2026-04-26 01:48:08 +0530

    Done

--- ORSProject-04/src/main/java/in/co/rays/proj4/Test/SubjectTest.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/Test/SubjectTest.java
@@ -1,17 +1,24 @@
 package in.co.rays.proj4.Test;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import in.co.rays.proj4.bean.SubjectBean;
+import in.co.rays.proj4.exception.ApplicationException;
 import in.co.rays.proj4.model.SubjectModel;
 
 public class SubjectTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ApplicationException {
 		// getAdd();
 		// getUpdate();
 		// getDelete();
+		// findByPk();
+		//findByName();
+		search();
 	}
 
 	public static void getAdd() {
@@ -62,6 +69,84 @@ public class SubjectTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void findByPk() {
+
+		try {
+			SubjectBean bean = new SubjectBean();
+			SubjectModel model = new SubjectModel();
+			bean = model.findByPk(1);
+
+			if (bean != null) {
+				System.out.println("Id: " + bean.getId());
+				System.out.println("Name: " + bean.getName());
+				System.out.println("Course Id: " + bean.getCourseId());
+				System.out.println("Course Name: " + bean.getCourseName());
+				System.out.println("Description: " + bean.getDescription());
+				System.out.println(bean.getCreatedBy());
+				System.out.println(bean.getModifiedBy());
+				System.out.println(bean.getCreatedDatetime());
+				System.out.println(bean.getModifiedDatetime());
+				System.out.println("-------------------------");
+			} else {
+				System.out.println("No result found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void findByName() {
+
+		try {
+			SubjectBean bean = new SubjectBean();
+			SubjectModel model = new SubjectModel();
+			bean = model.findByName("Data Structure");
+
+			if (bean != null) {
+				System.out.println("Id: " + bean.getId());
+				System.out.println("Name: " + bean.getName());
+				System.out.println("Course Id: " + bean.getCourseId());
+				System.out.println("Course Name: " + bean.getCourseName());
+				System.out.println("Description: " + bean.getDescription());
+				System.out.println(bean.getCreatedBy());
+				System.out.println(bean.getModifiedBy());
+				System.out.println(bean.getCreatedDatetime());
+				System.out.println(bean.getModifiedDatetime());
+				System.out.println("-------------------------");
+			} else {
+				System.out.println("No result found");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void search() throws ApplicationException {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+		List<SubjectBean> list = new ArrayList<SubjectBean>();
+		list = model.search(bean, 1, 5);
+		Iterator<SubjectBean> it = list.iterator();
+		while (it.hasNext()) {
+			bean = it.next();
+
+			System.out.println("Id: " + bean.getId());
+			System.out.println("Name: " + bean.getName());
+			System.out.println("Course Id: " + bean.getCourseId());
+			System.out.println("Course Name: " + bean.getCourseName());
+			System.out.println("Description: " + bean.getDescription());
+			System.out.println(bean.getCreatedBy());
+			System.out.println(bean.getModifiedBy());
+			System.out.println(bean.getCreatedDatetime());
+			System.out.println(bean.getModifiedDatetime());
+			System.out.println("-------------------------");
+		}
+
 	}
 
 }
--- ORSProject-04/src/main/java/in/co/rays/proj4/model/SubjectModel.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/model/SubjectModel.java
@@ -3,6 +3,8 @@ package in.co.rays.proj4.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import in.co.rays.proj4.bean.CourseBean;
 import in.co.rays.proj4.bean.MarksheetBean;
@@ -146,6 +148,130 @@ public class SubjectModel {
 			throw new ApplicationException("Exception in deleted Subject...");
 		}
 
+	}
+
+	public SubjectBean findByPk(long pk) throws ApplicationException {
+		StringBuffer sql = new StringBuffer("select * from st_subject where id = ?");
+		Connection conn = null;
+		SubjectBean bean = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+			pstmt.setLong(1, pk);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new SubjectBean();
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setCourseId(rs.getLong(3));
+				bean.setCourseName(rs.getString(4));
+				bean.setDescription(rs.getString(5));
+				bean.setCreatedBy(rs.getString(6));
+				bean.setModifiedBy(rs.getString(7));
+				bean.setCreatedDatetime(rs.getTimestamp(8));
+				bean.setModifiedDatetime(rs.getTimestamp(9));
+			}
+			rs.close();
+			pstmt.close();
+		} catch (Exception e) {
+			throw new ApplicationException("Exception : Exception in getting Subject by pk");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return bean;
+	}
+
+	public SubjectBean findByName(String name) throws ApplicationException {
+		StringBuffer sql = new StringBuffer("select * from st_subject where name = ?");
+		Connection conn = null;
+		SubjectBean bean = null;
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+			pstmt.setString(1, name);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new SubjectBean();
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setCourseId(rs.getLong(3));
+				bean.setCourseName(rs.getString(4));
+				bean.setDescription(rs.getString(5));
+				bean.setCreatedBy(rs.getString(6));
+				bean.setModifiedBy(rs.getString(7));
+				bean.setCreatedDatetime(rs.getTimestamp(8));
+				bean.setModifiedDatetime(rs.getTimestamp(9));
+			}
+			rs.close();
+			pstmt.close();
+		} catch (Exception e) {
+			throw new ApplicationException("Exception : Exception in getting Subject by Name");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+
+		return bean;
+	}
+	
+	public List<SubjectBean> search(SubjectBean bean, int pageNo, int pageSize) throws ApplicationException {
+		StringBuffer sql = new StringBuffer("select * from st_subject where 1=1");
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" and id = " + bean.getId());
+			}
+			if (bean.getName() != null && bean.getName().length() > 0) {
+				sql.append(" and name like '" + bean.getName() + "%'");
+			}
+			if (bean.getCourseId() > 0) {
+				sql.append(" and course_id = " + bean.getCourseId());
+			}
+			if (bean.getCourseName() != null && bean.getCourseName().length() > 0) {
+				sql.append(" and course_name like '" + bean.getCourseName() + "%'");
+			}
+			if (bean.getDescription() != null && bean.getDescription().length() > 0) {
+				sql.append(" and description like '" + bean.getDescription() + "%'");
+			}
+
+		}
+
+		if (pageNo > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			sql.append(" limit " + pageNo + ", " + pageSize);
+		}
+
+		ArrayList<SubjectBean> list = new ArrayList<SubjectBean>();
+		Connection conn = null;
+		try {
+			conn = JDBCDataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				bean = new SubjectBean();
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setCourseId(rs.getLong(3));
+				bean.setCourseName(rs.getString(4));
+				bean.setDescription(rs.getString(5));
+				bean.setCreatedBy(rs.getString(6));
+				bean.setModifiedBy(rs.getString(7));
+				bean.setCreatedDatetime(rs.getTimestamp(8));
+				bean.setModifiedDatetime(rs.getTimestamp(9));
+				list.add(bean);
+			}
+			rs.close();
+			pstmt.close();
+		} catch (Exception e) {
+			throw new ApplicationException("Exception : Exception in search Subject");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		return list;
 	}
 
 }
